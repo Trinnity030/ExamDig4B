0398460789e98f1b9d245aadfebb67f7863e7017
XimenaFlores <20233tn136@utez.edu.mx>
2024-09-04 22:10:55 -0600

    Inicio del proyecto

---
+++ script.js
@@ -0,0 +1,8 @@
+const parrafo = document.getElementById("descripcion");
+const boton = document.getElementById("btnCambiarColor");
+
+boton.addEventListener("click", function() {
+  parrafo.style.backgroundColor = "lightblue";
+});
+
+
